55168f2ccf7206ae57dfc08008faea6e3d647dd9
Julia Halbeisen <julia.halbeisen@gibmit.ch>
2018-05-16 16:11:19 +0200

    DTO_Figure

--- tetris/src/data/Figure.java
+++ tetris/src/data/Figure.java
@@ -6,4 +6,5 @@ import java.awt.Point;
 public class Figure {
 	protected Point[][] appereance = new Point[4][4];
 	protected Color color;
+	protected int rotation;
 }
--- tetris/src/dataTransferObject/DTO_Figure.java
+++ tetris/src/dataTransferObject/DTO_Figure.java
@@ -6,8 +6,12 @@ import java.awt.Point;
 import data.Figure;
 
 public class DTO_Figure extends Figure {
-	public Point[][] getAppereance() {
-		return this.appereance;
+	public Point[] getAppereance() {
+		Point[] appear = new Point[4];
+		for (int i = 0; i < appear.length; i++) {
+			appear[i] = this.appereance[rotation % 4][i];
+		}
+		return appear;
 	}
 	
 	public Color getColor() {
