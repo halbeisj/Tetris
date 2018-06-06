b294a3a6357faabec6e99ff72b4924c322ed636b
Julia Halbeisen <julia.halbeisen@gibmit.ch>
2018-06-06 15:36:41 +0200

    Game classstructure

--- tetris_v2/src/b_bl/Game.java
+++ tetris_v2/src/b_bl/Game.java
@@ -1,5 +1,35 @@
 package b_bl;
 
+import c_db.Figure;
+import d_dto.Point_DTO;
+
 public class Game {
+	private Point_DTO[][] field;
+	private Figure figure;
+	private Figure figure_next;
+	
+	public void left(){
+		
+	}
+	
+	public void right(){
+		
+	}
+	
+	public boolean down(){
+		return;
+	}
+	
+	public void createNewFigure(){
+		
+	}
+	
+	public void newGame(){
+		
+	}
+	
+	public void endGame(){
+		
+	}
 
 }
