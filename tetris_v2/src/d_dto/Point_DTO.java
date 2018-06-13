cb26aa87eec0fd755375b07f6ee0a14d365e933a
Julia Halbeisen <julia.halbeisen@gibmit.ch>
2018-06-13 13:53:15 +0200

    configure Point_DTO[][]

--- tetris_v2/src/d_dto/Point_DTO.java
+++ tetris_v2/src/d_dto/Point_DTO.java
@@ -3,40 +3,69 @@ package d_dto;
 import java.awt.Color;
 import java.awt.Point;
 
-/***/
+/**
+ * Diese Klasse wiederspiegelt einen Punkt auf dem Spielfeld
+ * 
+ * @author Julia Halbeisen, Dominik Heckendorn
+ * @version 1.0
+ * */
 public class Point_DTO {
 	private Point point;
 	private Color color;
 	private int status;
 	
-	/***/
+	/**
+	 * Diese Methode gibt den Punkt zurück, für welchen die weiteren Einstellungen gelten.
+	 * 
+	 * @return Punkt, welcher konfiguriert wird
+	 * */
 	public Point getPoint(){
-		return point;
+		return this.point;
 	}
 	
-	/***/
+	/**
+	 * Mit dieser Methode kann konfiguriert werden, für welchen Punkt die Einstellungen gelten.
+	 * 
+	 * @param Punkt, für welchen die Einstellungen gelten
+	 * */
 	public void setPoint(Point point){
-		
+		this.point = point;
 	}
 	
-	/***/
+	/**
+	 * Diese Methode gibt die Farbe des Punktes zurück
+	 * 
+	 * @return Farbe des Punktes
+	 * */
 	public Color getColor(){
-		return color;
+		return this.color;
 	}
 	
-	/***/
+	/**
+	 * Mit dieser Methode kann die Farbe des Punktes konfiguriert werden.
+	 * 
+	 * @param Farbe, welche konfiguriert werden soll
+	 * */
 	public void setColor(Color color){
-		
+		this.color = color;
 	}
 	
-	/***/
+	/**
+	 * Diese Methode gibt den Status (frei(0), besetzt(1), temp(2)) des Punktes zurück
+	 * 
+	 * @return Status des Punktes
+	 * */
 	public int getStatus(){
-		return status;
+		return this.status;
 	}
 	
-	/***/
-	public void setStatus(int Status){
-		
+	/**
+	 * Diese Methode setzt den Status (frei(0), besetzt(1), temp(2)) des Punktes
+	 * 
+	 * @param Status, welcher konfiguriert werden soll
+	 * */
+	public void setStatus(int status){
+		this.status = status;
 	}
 	
 
