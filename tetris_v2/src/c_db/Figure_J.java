717cfc7b02ee42c83dba79b4e08b25e19a50f3b3
Julia Halbeisen <julia.halbeisen@gibmit.ch>
2018-06-20 14:12:17 +0200

    Comments

--- tetris_v2/src/c_db/Figure_J.java
+++ tetris_v2/src/c_db/Figure_J.java
@@ -1,6 +1,10 @@
 package c_db;
 
-/***/
+/**Hier wird die Figur J erstellt, diese Klasse erbst von der Klasse Figure.
+ * @author Julia Halbeisen
+ * @author Dominik Heckendorn
+ * @version 1.0
+ * */
 public class Figure_J extends Figure{
 
 }
--- tetris_v2/src/c_db/Figure_L.java
+++ tetris_v2/src/c_db/Figure_L.java
@@ -1,6 +1,10 @@
 package c_db;
 
-/***/
+/**Hier wird die Figur L erstellt, diese Klasse erbst von der Klasse Figure.
+ * @author Julia Halbeisen
+ * @author Dominik Heckendorn
+ * @version 1.0
+ * */
 public class Figure_L extends Figure{
 
 }
